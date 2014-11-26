056828742857847b7a6f46fa5ae2ae05a70b7f11
alexwh <alexwh@users.noreply.github.com>
2014-11-26 16:40:23 +0000

    more reliably load the zkbd keyfile

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -1,6 +1,26 @@
 bindkey -e # I don't like vim bindings in my terminal
 
+# from http://zshwiki.org/home/zle/bindkeys
 autoload zkbd
+function zkbd_file() {
+	[[ -f ~/.zkbd/${TERM}-${VENDOR}-${OSTYPE} ]] && printf '%s' ~/".zkbd/${TERM}-${VENDOR}-${OSTYPE}" && return 0
+	[[ -f ~/.zkbd/${TERM}-${DISPLAY}          ]] && printf '%s' ~/".zkbd/${TERM}-${DISPLAY}"          && return 0
+	return 1
+}
+[[ ! -d ~/.zkbd ]] && mkdir ~/.zkbd
+keyfile=$(zkbd_file)
+ret=$?
+if [[ ${ret} -ne 0 ]]; then
+	zkbd
+	keyfile=$(zkbd_file)
+	ret=$?
+fi
+if [[ ${ret} -eq 0 ]] ; then
+	source "${keyfile}"
+else
+	printf 'Failed to setup keys using zkbd.\n'
+fi
+unfunction zkbd_file; unset keyfile ret
 source ~/.zkbd/$TERM-$DISPLAY
 
 bindkey '^r' history-incremental-search-backward
