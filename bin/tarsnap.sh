bc611ef58fe82dc6737f3ab109f7d316a93292da
alexwh <alexwh@users.noreply.github.com>
2015-01-10 22:14:13 +0000

    clean up tarsnap script

--- bin/tarsnap.sh
+++ bin/tarsnap.sh
@@ -1,22 +1,22 @@
 #!/usr/bin/env bash
+# this is a (heavily) modified/simplified version of https://github.com/Gestas/Tarsnap-generations
 
 notify() {
+	echo "$@"
 	DISPLAY=:0.0 sudo -u alex notify-send "Tarsnap" "$@"
 }
 
-date_bin=$(which date)
 # 'now' needs to be constant during execution
-now=$($date_bin +%s)
+now=$(date +%s)
 
 notify "Starting tarsnap backups..."
 
+# when for has no extra args, it defaults to "$@"
 for dir;do
-	tarsnap -v -c -f "${now}_${HOSTNAME}-${dir}" --one-file-system -C / $dir
-	if [ $? = 0 ];then
-		echo "${now}_${HOSTNAME}-${dir} backup done."
+	tarsnap -v -c -f "${now}_${HOSTNAME}-${dir}" --one-file-system -C / "$dir"
+	if [[ $? -eq 0 ]];then
 		notify "${now}_${HOSTNAME}-${dir} backup done."
 	else
-		echo "${now}_${HOSTNAME}-${dir} backup error. Exiting";exit $?
 		notify "${now}_${HOSTNAME}-${dir} backup error. Exiting";exit $?
 	fi
 done
@@ -25,27 +25,24 @@ archive_list=$(tarsnap --list-archives)
 
 # Check to make sure the last set of backups are OK.
 for dir;do
-	if grep -q "${now}_${HOSTNAME}-${dir}" <<< $archive_list;then
+	if grep -q "${now}_${HOSTNAME}-${dir}" <<< "$archive_list";then
 		echo "${now}_${HOSTNAME}-${dir} backup OK."
 	else
-		echo "${now}_${HOSTNAME}-${dir} backup NOT OK. Check --archive-list."; exit 3
 		notify "${now}_${HOSTNAME}-${dir} backup NOT OK. Check --archive-list."; exit 3
 	fi
 done
 
 # Delete backups older than a week
-delete_time=$($date_bin -d"-1 week" +%s)
+delete_time=$(date -d"-1 week" +%s)
 
 for backup in $archive_list;do
-	backup_time=$(cut -d _ -f1 <<< $backup)
+	backup_time=$(cut -d _ -f1 <<< "$backup")
 
 	if [[ $backup_time -lt $delete_time ]];then
-		tarsnap -d -f $backup
-		if [ $? = 0 ];then
-			echo "$backup snapshot deleted."
+		tarsnap -d -f "$backup"
+		if [[ $? -eq 0 ]];then
 			notify "$backup snapshot deleted."
 		else
-			echo "Unable to delete $backup. Exiting";exit $?
 			notify "Unable to delete $backup. Exiting";exit $?
 		fi
 	fi
