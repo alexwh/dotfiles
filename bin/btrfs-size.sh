274640ffc56b2a991b297a68610d1950c3493572
alexwh <alexwh@users.noreply.github.com>
2015-01-10 22:53:29 +0000

    remove useless scripts

--- bin/btrfs-size.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-used=$(sudo  btrfs filesystem show /dev/mapper/root | grep Total | awk '{print $7}' | tr -d A-Za-z)
-total=$(sudo btrfs filesystem show /dev/mapper/root | grep path  | awk '{print $4}' | tr -d A-Za-z)
-unit=$(sudo  btrfs filesystem show /dev/mapper/root | grep Total | awk -F'[0-9]' '{print $NF}')
-
-[[ $1 == "int" ]] && echo $(bc <<< "($total-$used)/1") || echo $(bc <<< "$total-$used")$unit
--- bin/filelistbackup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-find / | aescrypt -e -p "$(< /home/alex/.config/aescrypt.pass)" -o /home/alex/sync/misc/txt/$(hostname)filelist.txt.aes -
