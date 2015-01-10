974f1160a031e1863e5c38f6455459c276d7637c
alexwh <alexwh@users.noreply.github.com>
2015-01-10 22:46:20 +0000

    clean up

--- bin/backupssd.sh
+++ bin/backupssd.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
 backuppath="/media/data/backups/ssd"
-opts="-aAXvP --del"
 
 start=$(date +%s)
-rsync $opts /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/chromium/*,/home/*/.cache}
+rsync -aAXvP --del /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/chromium/*,/home/*/.cache}
 end=$(date +%s)
-echo -e "took $(( end-start )) seconds\n\
-date '+done at %F %T'"> $backuppath/backupinfo
+echo -e "took $(( end-start )) seconds\n$(date '+done at %F %T')" > $backuppath/backupinfo
 pacman -Qqn > $backuppath/basepkgs
 pacman -Qqm > $backuppath/aurpkgs
 pacman -Qqe > $backuppath/explpkgs
