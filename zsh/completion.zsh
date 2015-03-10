78fd83398eb0baec0297865f1fdb8594ecaed5a2
alexwh <alexwh@users.noreply.github.com>
2015-03-10 21:21:18 +0000

    tidy completion.zsh
    be consistent with autoload -Uz, compinit/zmodload -i is harmful (blame
    zshuery), completion dots didn't work nor were useful

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,17 +1,8 @@
-autoload -U compinit
-compinit -i
-compdef mcd=cd
-zmodload -i zsh/complist
+autoload -Uz compinit && compinit
+zmodload zsh/complist
 
 zstyle ':completion:*' menu select
-zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-colors '' # use ls colors
 zstyle ':completion:*' matcher-list 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=*' 'l:|=* r:|=*'
-expand-or-complete-with-dots() {
-	echo -n "\e[31m......\e[0m"
-	zle expand-or-complete
-	zle redisplay
-}
-zle -N expand-or-complete-with-dots
-bindkey "^I" expand-or-complete-with-dots
 
 compdef -d mpv
