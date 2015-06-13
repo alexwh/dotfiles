6f80088447e96edb5bd61db0126cea10e5b79bb1
alexwh <alexwh@users.noreply.github.com>
2015-06-13 01:26:28 +0100

    remove superflous chrome pattern

--- bin/backupssd.sh
+++ bin/backupssd.sh
@@ -3,7 +3,7 @@
 backuppath="/media/data/backups/ssd"
 
 start=$(date +%s)
-rsync -aAXvP --del /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/chromium/*,/home/*/.cache}
+rsync -aAXvP --del /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/*}
 end=$(date +%s)
 echo -e "took $(( end-start )) seconds\n$(date '+done at %F %T')" > $backuppath/backupinfo
 pacman -Qqn > $backuppath/basepkgs
