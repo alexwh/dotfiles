6c9cc50cf7f996db79d9ed55e8f2fe2788e09efe
alexwh <alexwh@users.noreply.github.com>
2015-08-15 01:58:59 +0100

    add scp ranger command

---
+++ config/ranger/commands.py
@@ -0,0 +1,32 @@
+from ranger.api.commands import *
+
+class scp(Command):
+    def execute(self):
+        if self.arg(1):
+            scpcmd = ["scp", "-r"]
+            scpcmd.extend([f.realpath for f in self.fm.thistab.get_selection()])
+            scpcmd.append(self.arg(1))
+            self.fm.execute_command(scpcmd)
+            self.fm.notify("Uploaded!")
+
+
+    def tab(self):
+        import os.path
+        try:
+            import paramiko
+        except ImportError:
+            """paramiko not installed"""
+            return
+
+        try:
+            with open(os.path.expanduser("~/.ssh/config")) as file:
+                paraconf = paramiko.SSHConfig()
+                paraconf.parse(file)
+        except IOError:
+            """cant open ssh config"""
+            return
+
+        hosts = paraconf.get_hostnames()
+        # remove any wildcard host settings since they're not real servers
+        hosts.discard("*")
+        return (self.start(1) + host + ":" for host in hosts)
