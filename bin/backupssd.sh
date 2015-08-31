f9c643a88fd12fa8baedf5d2feeff93d104986db
alexwh <alexwh@users.noreply.github.com>
2015-08-31 03:31:50 +0100

    change backup path and log rsync output

--- bin/backupssd.sh
+++ bin/backupssd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-backuppath="/media/data/backups/ssd"
+backuppath="/data/backups/ssd"
 
 start=$(date +%s)
-rsync -aAXvP --del /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/*}
+rsync -aAXvP --del /* $backuppath/ --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/data/*,/lost+found,/home/*/.gvfs,/home/*/.thumbnails/*,/home/*/.cache/*} | tee $backuppath/rsyncoutput
 end=$(date +%s)
 echo -e "took $(( end-start )) seconds\n$(date '+done at %F %T')" > $backuppath/backupinfo
 pacman -Qqn > $backuppath/basepkgs
