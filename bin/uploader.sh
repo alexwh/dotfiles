40cf3e21b87e5f3523223fef503a0ae0739bfb25
alexwh <alexwh@users.noreply.github.com>
2015-11-14 22:46:43 +0000

    get full file link from imgur

--- bin/uploader.sh
+++ bin/uploader.sh
@@ -5,8 +5,7 @@ imgur() {
 	# TODO: use imgur account instead of app secret
 	response=$(curl -sw '\n' -H "Authorization: Client-ID fdd3b8a687cc481" -F "image=@$1" "https://api.imgur.com/3/upload.xml")
 
-	# TODO: don't hardcode to png - imgur auto jpgs over 1mb
-	url="http://i.imgur.com/$(sed -r 's|.*<id>(.*)</id>.*|\1|' <<< $response).png"
+	url="$(sed -r 's|.*<link>(.*)</link>.*|\1|' <<< $response)"
 	deleteurl="http://imgur.com/delete/$(sed -r 's|.*<deletehash>(.*)</deletehash>.*|\1|' <<< $response)"
 	uploadtime=$(date +%F_%T)
 
