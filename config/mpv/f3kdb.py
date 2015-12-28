946e0ef2ca9e8fad35fd73cdb36cf73d6379cdc1
alexwh <alexwh@users.noreply.github.com>
2015-12-28 17:03:05 +0000

    remove vapoursynth

--- config/mpv/f3kdb.py
+++
@@ -1,7 +0,0 @@
-import vapoursynth as vs
-core = vs.get_core()
-
-clip = video_in
-clip = core.std.Trim(clip, first=0, length=500000)
-clip = core.f3kdb.Deband(clip, grainy=0, grainc=0, output_depth=16)
-clip.set_output()
