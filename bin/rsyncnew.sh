a6b9344d08e7ffe5428450a9ab6b480ae2d4c6ea
alexwh <alexwh@users.noreply.github.com>
2016-02-29 02:42:11 +0000

    add rsyncnew.sh
    oneliner I pull out often and wanted to shorten

---
+++ bin/rsyncnew.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -o nounset # ie set -u
+set -o errexit # ie set -e
+server="$1"
+src="$2"
+dest="$3"
+
+# -r needs to be explicit with --files-from
+# %P in find is just the basepath of the match (so we don't create massive folder structure in $dest)
+rsync -ar --files-from=<(ssh "$server" find "$src" -mtime -1 -maxdepth 1 -mindepth 1 -printf '%P\\n') "$server":"$src" "$dest"
