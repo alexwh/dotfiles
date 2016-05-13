e6723faefb757ef3a45fc0b510a6102846bd34d1
alexwh <alexwh@users.noreply.github.com>
2016-05-14 00:50:43 +0100

    edit rsync options
    -a is too much (copies uid/gid)

--- bin/rsyncnew.sh
+++ bin/rsyncnew.sh
@@ -7,4 +7,4 @@ dest="$3"
 
 # -r needs to be explicit with --files-from
 # %P in find is just the basepath of the match (so we don't create massive folder structure in $dest)
-rsync -arv --files-from=<(ssh "$server" find "$src" -mtime -1 -maxdepth 1 -mindepth 1 -printf '%P\\n') "$server":"$src" "$dest"
+rsync -rptv --files-from=<(ssh "$server" find "$src" -mtime -1 -maxdepth 1 -mindepth 1 -printf '%P\\n') "$server":"$src" "$dest"
