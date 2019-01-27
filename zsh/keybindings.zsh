4aef86b6897705ce4976caaf810a698825a74819
alexwh <alexwh@users.noreply.github.com>
2019-01-27 19:37:22 +0000

    add vim textobject binds (ci", ci{, etc)

--- zsh/keybindings.zsh
+++ zsh/keybindings.zsh
@@ -40,6 +40,34 @@ bindkey '^[[Z' reverse-menu-complete # shift-tab
 bindkey '^[[1;5C' forward-word # ctrl+right
 bindkey '^[[1;5D' backward-word # ctrl+left
 
+# extra vim textobjects
+# ci"
+autoload -U select-quoted
+zle -N select-quoted
+for m in visual viopp; do
+  for c in {a,i}{\',\",\`}; do
+    bindkey -M $m $c select-quoted
+  done
+done
+
+# ci{, ci(
+autoload -U select-bracketed
+zle -N select-bracketed
+for m in visual viopp; do
+  for c in {a,i}${(s..)^:-'()[]{}<>bB'}; do
+    bindkey -M $m $c select-bracketed
+  done
+done
+
+# surround
+autoload -Uz surround
+zle -N delete-surround surround
+zle -N add-surround surround
+zle -N change-surround surround
+bindkey -a cs change-surround
+bindkey -a ds delete-surround
+bindkey -a ys add-surround
+bindkey -M visual S add-surround
 
 # Restore some keymaps removed by vim keybind mode
 bindkey '^P' up-history
