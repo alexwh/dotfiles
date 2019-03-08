ea13a563eabbbbb83c44b6691be521aa25a5c4c7
alexwh <alexwh@users.noreply.github.com>
2019-03-08 17:47:56 +0000

    fix zle -N syntax
    cannot load multiple zle widgets in one argument, the syntax is `zle -N
    widget [ function ]`

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -5,7 +5,8 @@ SAVEHIST="1000000"
 autoload -Uz zmv
 autoload -Uz colors && colors
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
-zle -N up-line-or-beginning-search down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
 autoload -Uz url-quote-magic
 zle -N self-insert url-quote-magic
 unset zle_bracketed_paste
