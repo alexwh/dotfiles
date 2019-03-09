deeedc9b87a22171d7c3563a71f583d70c1337e3
alexwh <alexwh@users.noreply.github.com>
2019-03-09 01:28:58 +0000

    remove old unused scripts

--- bin/pacmanlistbackup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-pacman -Qeq > /home/alex/sync/misc/txt/$(hostname)packages.txt
-echo -e "\n" >> /home/alex/sync/misc/txt/$(hostname)packages.txt
-pacman -Qmq >> /home/alex/sync/misc/txt/$(hostname)packages.txt
