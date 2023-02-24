33d6923d5c3d9b5678cdc0846cdbfa788b2b44aa
Alex W <alexwh@users.noreply.github.com>
2023-02-24 19:16:39 +0000

    Revert "pure -> starship"
    This reverts commit 0ed22c1ac2b38d1286267d1f892b6d29d1d3efba.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,5 +1,4 @@
 fpath=($fpath ~/.zsh)
-# export PURE_GIT_PULL=0
-# autoload -Uz promptinit && promptinit
-# prompt pure
-eval "$(starship init zsh)"
+export PURE_GIT_PULL=0
+autoload -Uz promptinit && promptinit
+prompt pure
