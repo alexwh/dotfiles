0f7167b7ea37b869ab6ac701b3c5afdf0a7bb3ed
Alex W <alexwh@users.noreply.github.com>
2023-08-05 22:00:24 +0100

    add friendly-snippets

--- vim/lua/lsp.lua
+++ vim/lua/lsp.lua
@@ -1,3 +1,22 @@
 local lsp = require('lsp-zero')
 lsp.preset("recommended")
 lsp.setup()
+
+-- Make sure you setup `cmp` after lsp-zero
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+require('luasnip.loaders.from_vscode').lazy_load()
+
+cmp.setup({
+  sources = {
+    {name = 'nvim_lsp'},
+    {name = 'luasnip'},
+  },
+  mapping = {
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  }
+})
