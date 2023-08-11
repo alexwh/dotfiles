47ccca29f68cbdb5241d02f4379fb76864761af5
Alex W <alexwh@users.noreply.github.com>
2023-08-11 12:34:23 +0100

    disable tree sitter indent for yaml

--- vim/lua/plugins.lua
+++ vim/lua/plugins.lua
@@ -88,6 +88,9 @@ require("lazy").setup {
         indent = {
           enable = true,
           disable = function(lang, buf)
+            if lang == "yaml" then
+              return true
+            end
             local max_filesize = 100 * 1024 -- 100 KB
             local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
