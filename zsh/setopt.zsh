94a60179728b6b8c4be732c1ef63742cb19b998c
Alex W <alexwh@users.noreply.github.com>
2023-09-08 16:52:18 +0100

    remove zsh url-quote-magic

--- zsh/setopt.zsh
+++ zsh/setopt.zsh
@@ -9,9 +9,6 @@ autoload -Uz colors && colors
 autoload -Uz up-line-or-beginning-search down-line-or-beginning-search
 zle -N up-line-or-beginning-search
 zle -N down-line-or-beginning-search
-autoload -Uz url-quote-magic
-zle -N self-insert url-quote-magic
-unset zle_bracketed_paste
 
 unsetopt flowcontrol
 
