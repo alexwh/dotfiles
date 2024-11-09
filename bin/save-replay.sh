857ac9a83fc7dffbc7fbe2f6090c07b41eb7f80b
Alex W <alexwh@users.noreply.github.com>
2024-11-09 20:04:39 +0000

    only show save replay notif if failed

--- bin/save-replay.sh
+++ bin/save-replay.sh
@@ -1,5 +1,4 @@
 #!/bin/sh -e
 
-killall -SIGUSR1 gpu-screen-recorder && \
-notify-send -t 5000 -u low -- "GPU Screen Recorder" "Replay saved" || \
+killall -SIGUSR1 gpu-screen-recorder || \
 notify-send -t 5000 -u low -- "GPU Screen Recorder" "Replay failed to save"
