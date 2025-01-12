913234b5acb6b821755079d77f7a54509b8bc686
Alex W <alexwh@users.noreply.github.com>
2025-01-12 01:31:57 +0000

    Revert "alias nvim to add +only -o"
    This reverts commit 23e2c7f027635d57cca2d1c7e3efa6bf7de90060.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -4,7 +4,6 @@ alias si='sudo -i'
 
 alias ls='eza -s extension --git'
 alias latr='eza -lastime --git'
-alias nvim='nvim +only -o '
 if [[ $(command -v nvim) ]];then
     alias vi='nvim'
     alias vim='nvim'
