31b1ac9fa7c9cec88408ad02b9681217177d0ca3
Alex W <alexwh@users.noreply.github.com>
2025-02-11 16:17:32 +0000

    xdotool -> kdotool for wayland

--- bin/record-save-application-name.sh
+++ bin/record-save-application-name.sh
@@ -3,8 +3,8 @@
 # This script should be passed to gpu-screen-recorder with the -sc option, for example:
 # gpu-screen-recorder -w screen -f 60 -a default_output -r 60 -sc scripts/record-save-application-name.sh -c mp4 -o "$HOME/Videos"
 
-window=$(xdotool getwindowfocus)
-window_name=$(xdotool getwindowclassname "$window" || xdotool getwindowname "$window" || echo "Desktop")
+window=$(kdotool getactivewindow)
+window_name=$(kdotool getwindowclassname "$window" || kdotool getwindowname "$window" || echo "Desktop")
 window_name="$(echo "$window_name" | tr '\0/\\' '_')"
 
 video_dir="$HOME/Videos/shadowplay/$window_name"
